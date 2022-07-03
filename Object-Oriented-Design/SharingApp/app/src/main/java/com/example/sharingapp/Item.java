7bc240b82e2f07047527b367c64a07d5fb96bf5d
Adekunle Afolabi <32042328+RHeX700@users.noreply.github.com>
2022-07-03 14:45:58 +0100

    Update

--- Object-Oriented-Design/SharingApp/app/src/main/java/com/example/sharingapp/Item.java
+++ Object-Oriented-Design/SharingApp/app/src/main/java/com/example/sharingapp/Item.java
@@ -17,7 +17,7 @@ public class Item {
     private String description;
     private Dimensions dimensions;
     private String status;
-    private String borrower;
+    private Contact borrower;
     protected transient Bitmap image;
     protected String image_base64;
     private String id;
@@ -90,11 +90,11 @@ public class Item {
         return status;
     }
 
-    public void setBorrower(String borrower) {
+    public void setBorrower(Contact borrower) {
         this.borrower = borrower;
     }
 
-    public String getBorrower() {
+    public Contact getBorrower() {
         return borrower;
     }
 
